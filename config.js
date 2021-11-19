8dfdaa577ff400734705256813f3e2973cd030fd
Abuzar <i190531@nu.edu.pk>
2021-11-18 22:04:23 -0800

    Deploy-2

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb+srv://pokemon:pokemon@cluster0.ttzsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'facebook': {
+        clientId: '296176445614772',
+        clientSecret: 'a493d6755b4f92a71a6f8b1f3d95411e'
+    }
+}
